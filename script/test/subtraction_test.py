54a17fae4638a8426cf17f9a513d0ddbe03e8927
Chase Kunz <chasekunz@inertialsense.com>
2019-11-11 12:19:02 -0700

    fail pytest

--- script/test/subtraction_test.py
+++ script/test/subtraction_test.py
@@ -6,4 +6,4 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'subtraction'))
 from subtraction import subtract
 
 def test_subtraction():
-    assert subtract(1,1) == 0
+    assert subtract(1,1) == 1
